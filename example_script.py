7877a57b984eeeb08ab0ce12950945842c65dadb
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-11 16:08:53 -0500

    Refactor get_bgp_neighbors_detail method

--- example_script.py
+++ example_script.py
@@ -1,11 +1,13 @@
 from napalm import get_network_driver
+import json
 
 driver = get_network_driver("sros")
 optional_args = {'port': 830}
-device = driver("127.0.0.1", "admin", "admin", 60, optional_args)
+device = driver("192.168.121.102", "admin", "admin", 60, optional_args)
 device.open()
-print(device.get_facts())
-print(device.get_optics())
+# print(device.get_facts())
+# print(device.get_optics())
+# print(device.get_environment())
+print(json.dumps(device.get_bgp_neighbors_detail(), indent=2))
+# print(device.get_bgp_neighbors_detail())
 device.close()
-
-
---
+++ napalm_sros/api/__init__.py
@@ -0,0 +1,1 @@
+from .get_bgp_neighbors_detail import get_bgp_neighbors_detail
