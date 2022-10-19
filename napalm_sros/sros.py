20e67f229c49f5f70a0386ad7c530d17e771c4ce
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-19 00:21:05 -0500

    Need to throw error when there is one, not eat it

--- napalm_sros/sros.py
+++ napalm_sros/sros.py
@@ -443,45 +443,41 @@ class NokiaSROSDriver(NetworkDriver):
             with open(filename) as f:
                 configuration = f.read()
 
-        try:
-            self.fmt = self._determinne_config_format(configuration)
-            if self.fmt == "xml":
-                if not self.lock_disable and not self.session_config_lock:
-                    self._lock_config()
-                configuration = etree.XML(configuration)
-                configuration_tree = etree.ElementTree(configuration)
-                root = configuration_tree.getroot()
-                if root.tag != "{urn:ietf:params:xml:ns:netconf:base:1.0}config":
-                    newroot = etree.Element(
-                        "{urn:ietf:params:xml:ns:netconf:base:1.0}config"
-                    )
-                    newroot.insert(0, root)
-                    self.conn.edit_config(
-                        config=newroot, target="candidate", default_operation="merge"
-                    )
-
-                else:
-                    self.conn.edit_config(
-                        config=configuration,
-                        target="candidate",
-                        default_operation="merge",
-                    )
-                self.conn.validate(source="candidate")
+        self.fmt = self._determinne_config_format(configuration)
+        if self.fmt == "xml":
+            if not self.lock_disable and not self.session_config_lock:
+                self._lock_config()
+            configuration = etree.XML(configuration)
+            configuration_tree = etree.ElementTree(configuration)
+            root = configuration_tree.getroot()
+            if root.tag != "{urn:ietf:params:xml:ns:netconf:base:1.0}config":
+                newroot = etree.Element(
+                    "{urn:ietf:params:xml:ns:netconf:base:1.0}config"
+                )
+                newroot.insert(0, root)
+                self.conn.edit_config(
+                    config=newroot, target="candidate", default_operation="merge"
+                )
 
             else:
-                configuration = configuration.split("\n")
-                configuration.insert(0, "edit-config exclusive")
-                configuration.append("quit-config")
-                buff = self._perform_cli_commands(configuration, False)
-                # error checking
-                if buff is not None:
-                    for item in buff.split("\n"):
-                        if any(match.search(item) for match in self.terminal_stderr_re):
-                            raise MergeConfigException("Merge issue: %s", item)
+                self.conn.edit_config(
+                    config=configuration,
+                    target="candidate",
+                    default_operation="merge",
+                )
+            self.conn.validate(source="candidate")
 
-        except MergeConfigException as me:
-            print("Merge issue : {}".format(me))
-            log.error("Merge issue : %s" %traceback.format_exc())
+        else:
+            configuration = configuration.split("\n")
+            configuration.insert(0, "edit-config exclusive")
+            configuration.append("quit-config")
+            buff = self._perform_cli_commands(configuration, False)
+            # error checking
+            if buff is not None:
+                for item in buff.split("\n"):
+                    if any(match.search(item) for match in self.terminal_stderr_re):
+                        log.error(f"Merge issue : {item}")
+                        raise MergeConfigException("Merge issue: %s", item)
 
     def load_replace_candidate(self, filename=None, config=None):
         """
