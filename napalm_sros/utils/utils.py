d34453e0db5651082b57ec5f9efddae05f0a95fc
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2022-10-31 15:58:04 -0500

    Use a package specific logger, not root

--- napalm_sros/utils/utils.py
+++ napalm_sros/utils/utils.py
@@ -3,10 +3,9 @@ from datetime import datetime
 
 LOG_FORMAT = '%(asctime)-15s %(filename)s %(funcName)s line %(lineno)d %(levelname)s:  %(message)s'
 
-
 def init_logging():
     date = datetime.now()
     new_date = date.strftime('%Y-%m-%d %H.%M.%S')
     file_path = "./logs_{}.log".format(new_date)
     logging.basicConfig(filename=file_path, format=LOG_FORMAT, level=logging.DEBUG)
-    return logging.getLogger()
+    return logging.getLogger("napalm.sros") # Return a specific logger, not root
